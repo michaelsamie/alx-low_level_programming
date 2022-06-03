fba762075840a85edef8e133ed1e4355a747e632
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-02 20:18:46 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- *C program using puts instead of printf and return 0  
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
