6404ad1fcaf7ce1222132ba9ff0548ee3384f98f
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-03 20:41:03 -0700

    Patience, persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main- A program that print possible cobination of number separated by comma
+*main- A program that print sentence in lowercase without q and e new line
 *Return: Always 0
 */
 int main(void)
 {
-int n;
+char n;
+
 for  (n = 48; n <= 57; n++)
 	putchar(n);
 	if (n != 57)
@@ -16,3 +17,4 @@ for  (n = 48; n <= 57; n++)
 putchar(10);
 return (0);
 }
+
