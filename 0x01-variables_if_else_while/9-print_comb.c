9982d9efc0a6cd7ae82f7cee902179d03b581d44
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-03 21:00:00 -0700

    Patience, persistence

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 char ch;
-for (ch = 48; ch <= 57; ch++)
+for (ch = 48; ch <= 57 ch++)
 	putchar(ch);
-	if (ch != 57)
-		putchar(44);
-		putchar(32);
+if (ch != 57)
+	putchar(44);
+	putchar(32);
 putchar(10);
 return (0);
 }
