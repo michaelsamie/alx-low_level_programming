380a299cc134248d729c2d351438f1797afbdc6d
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-03 20:14:28 -0700

    7.  Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main- A program that print hexidecimal with lowercase number in lowercase
+*Return: Always 0
+*/
+int main(void)
+{
+char ch;
+for  (ch = 48; ch <= 57; ch++)
+	putchar(n);
+for (ch = 97; ch <= 102; ch++)
+	putchar(ch);
+putchar(10);
+return (0);
+}
+
