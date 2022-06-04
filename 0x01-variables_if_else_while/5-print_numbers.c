0e9d7e2c9a2f1fa1b3be35361748c774b90dfe39
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-03 19:40:44 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main- A program that print number less than ten
+*Return: Always 0
+*/
+int main(void)
+{
+int n;
+for  (n = 0; n < 10; n++)
+	printf("%d", n);
+return (0);
+}
