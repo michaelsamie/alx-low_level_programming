b34e5bc5ffed6800d2b79121dcc202f9888f16eb
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-07 16:03:03 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main- A program that print sentence in lowercase
+*Return: Always 0
+**/
+void print_alphabet(void)
+{
+	char ch;
+
+	for  (ch = 97; ch <= 122; ch++)
+		_putchar(ch);
+_putchar(10);
+}
