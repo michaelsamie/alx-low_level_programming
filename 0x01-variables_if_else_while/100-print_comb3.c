698c3b5221e4726dfdec5d131a64f7b99a912067
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-06 20:53:28 -0700

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main- A program that print sentence in lowercase  and combine two numbers
+*Return: Always 0
+*/
+int main(void)
+{
+int m;
+int n;
+for  (m = 48; m <= 57; m++)
+	for (n = 49; n <= 57; n++)
+		if (n > m)
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+				putchar(44);
+				putchar(32);
+putchar(10);
+
+return (0);
+}
+
