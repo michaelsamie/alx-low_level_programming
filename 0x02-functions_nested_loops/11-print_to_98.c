f7d4c4d77191327f4216a015c4d0b40ffaeceaa9
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-08 22:25:50 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_to_98 - sum two numbers
+*@n : parameter to deternmin sum
+*Return: Always 0
+*/
+void print_to_98(int n)
+{
+	for (n >= 0; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+	return (0);
+}
