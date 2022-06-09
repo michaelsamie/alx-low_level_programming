c4346df8b40296ff3045c0aba8adc3e50782c347
micahelsamie <adesanmiadejuwon@gmail.com>
2022-06-08 21:56:37 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*add - sum two numbers
+*@n @j : parameter to deternmine sum
+*Return: return  sum o the number.
+*/
+int add(int, int)
+{
+	int n, j, p;
+
+	p = n + j;
+	return (p);
+}
