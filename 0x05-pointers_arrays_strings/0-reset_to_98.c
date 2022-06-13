286007f4fb5433c66a2efd8cfcd26b76bb959731
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-13 15:27:49 -0700

    reset project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * * reset_to_98 - a function to update n;
- * *
+ * *@n : poiter to n
  * * Return: Always nothing.
  */
-void reset_to_98(int *r)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
