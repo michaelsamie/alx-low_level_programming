b903ea046111d44a13e56582d42550c43dbed4a4
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-16 13:31:21 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strncat - check and print reverse
+ * *@src: paramter
+ * *@dest: paramter
+ * *@n: paramter
+ * *Return: a string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest);
+}
+
