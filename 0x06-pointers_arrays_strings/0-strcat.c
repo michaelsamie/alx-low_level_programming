c73bdfe19207b8e8fbca7394bf03aa5ff74f5ab3
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-16 13:43:29 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<string>
 /**
  * *_strcat - check and print reverse
  * *@src: paramter
@@ -8,15 +9,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
-
-	while (dest[len])
-		len++;
-	for (i = 0; src[i] != '0'; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+	strncat(dest, src);
 	return (dest);
 }
+
