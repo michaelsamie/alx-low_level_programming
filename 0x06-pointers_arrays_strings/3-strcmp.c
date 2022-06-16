ff481e009429ddda3df3dc9c2375d4e772ebfe79
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-16 14:29:39 -0700

    compare task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<string.h>
+/**
+ * *_strcmp - check and print reverse
+ * *@s1: paramter
+ * *@s2: paramter
+ * *Return: an integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int answer;
+
+	answer = strcmp(s1, s2);
+	return (answer);
+}
+
