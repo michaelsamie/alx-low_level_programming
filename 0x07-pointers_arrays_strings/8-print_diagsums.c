48026d7fe986044270387cf3b4be5457ad2e338a
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-21 15:08:22 -0700

    8-print task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,21 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * * print_diagsums - two diagonals of square matrix
+ * * @a: matrix
+ * * @size: size
+ * * Return: Nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+	int b, suma = 0, sumb = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		suma += a[(size + 1) * b];
+		sumb += a[(size - 1) * (b + 1)];
+	}
+
+	printf("%d, %d\n", suma, sumb);
+}
