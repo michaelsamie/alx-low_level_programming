5dcf25aad5eb3e8b06562d88433453bd5cebfbdc
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-24 21:42:45 -0700

    arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * * main -> function arguments it receives.
+ * * @argc: parameter
+ * * @argv: an array
+ * * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, j, answer = 0;
+	int spill[] = {15, 7, 8, 3, 2};
+
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && n >= 0; j++)
+	{
+		while (n >= spill[j])
+		{
+			n -= spill[j];
+			answer++;
+		}
+	}
+	printf("%d\n", answer);
+	return (0);
+}
