8f7c4edfe3f8a0ab00a37101d751dff667657f4e
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-24 21:28:38 -0700

    whatmyname task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,10 +4,10 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
- * * is_palindrome - checks string 
+ * * is_palindrome - checks string
  * * @s: reverse
  * * Return: 1 or 0
- * */
+ */
 int is_palindrome(char *s)
 {
 	if (*s == 0)
@@ -17,8 +17,7 @@ int is_palindrome(char *s)
 
 /**
  * * _strlen_recursion - returns the length of a string
- * * @s: string 
- * *
+ * * @s: string
  * * Return: interger
  */
 int _strlen_recursion(char *s)
@@ -29,7 +28,7 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * * check_pal - checks the characters 
+ * * check_pal - checks the characters
  * * @s: string
  * * @i: counter
  * * @len: string lenght
