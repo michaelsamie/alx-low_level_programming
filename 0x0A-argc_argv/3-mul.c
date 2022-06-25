f39fa7ea3d9f0e773a6b52093ef341b8e2d3a7ba
michaelsamie <adesanmiadejuwon@gmail.com>
2022-06-24 21:54:40 -0700

    3ml task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-/**
- * * main -> function arguments it receives.
- * * @argc: parameter
- * * @argv: an array
- * * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
-}
