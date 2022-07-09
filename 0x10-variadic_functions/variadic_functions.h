00d20e705dc8d08953a2868a377e9f3f517e85c6
michaelsamie <adesanmiadejuwon@gmail.com>
2022-07-09 07:22:47 -0700

    sum of all its parameters.

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIADIC_FUNCTIONS_H_INCLUDED
+#define VARIADIC_FUNCTIONS_H_INCLUDED
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif // VARIADIC_FUNCTIONS_H_INCLUDED
